59ad79e95aaaf28695ded03a79d62288a690a9b2
Shawn Cicoria <github@cicoria.com>
2026-05-24 18:38:25 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,12 +5,16 @@ int main(int argc, char *argv[])
 {
   // Flush after every printf
   setbuf(stdout, NULL);
-  printf("$ ");
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  // Remove the trailing newline
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+
+  while(1)
+  {
+    printf("$ ");
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    // Remove the trailing newline
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
